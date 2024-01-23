4f63e1634feab4dd38a5deb759b32773e6ef75d6
Mackenly Jones <53151058+mackenly@users.noreply.github.com>
2024-01-22 23:35:46 -0500

    Swap jest for vitest

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,26 @@
-import app from '.';
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import type { UnstableDevWorker } from "wrangler";
+import { unstable_dev } from "wrangler";
 
-describe('Test the application', () => {
-  it('Should return 200 response', async () => {
-    const res = await app.request('http://localhost/')
-    expect(res.status).toBe(200)
-  })
-})
+describe("Worker", () => {
+
+  let worker: UnstableDevWorker;
+
+  beforeAll(async () => {
+    worker = await unstable_dev("src/index.ts", {
+      experimental: { disableExperimentalWarning: false },
+    });
+  });
+
+  afterAll(async () => {
+    await worker.stop();
+  });
+
+  it("should return 200 response", async () => {
+    const response = await worker.fetch(`http://${worker.address}:8787/`);
+    expect(response.status).toBe(200);
+    expect(response.headers.get("content-type")).toBe("text/plain;charset=UTF-8");
+    const responseBody: any = await response.text(); // text because we return c.text('Hello World!');
+    expect(responseBody).toBe("Hello World!");
+  });
+});
